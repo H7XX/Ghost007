087c18df0a2a400c16d1812bdba368588aee85b8
H7XX <88547689+H7XX@users.noreply.github.com>
2021-08-06 19:34:43 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,15 +8,9 @@ sudo apt-get install figlet curl nmap grep t50 php pip python python2 python3 ai
 
 pip install pynput
 
-chmod +x requisitos/scanport.sh
-chmod +x requisitos/ipinfo.sh
-chmod +x requisitos/DosConfig.sh
-chmod +x requisitos/slowconfig.sh
-chmod +x requisitos/t50config.sh
-chmod +x requisitos/SpamBot.py
-chmod +x requisitos/WifiDos.sh
-chmod +x requisitps/wifibrute.sh
-chmod +x Ghost007.sh
+cd requisitos/
+
+chmod +x *
 
 clear
 
