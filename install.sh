2c262554ef6f9afecff09f24c9880976a366c5a4
H7XX <88547689+H7XX@users.noreply.github.com>
2021-08-10 18:45:00 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,19 +4,19 @@ clear
 
 echo "instalando. . ."
 
-sudo apt-get install figlet curl nmap grep t50 php pip python python2 python3 aircrack-ng dirb metasploit-framework -y
+sudo apt-get install figlet curl nmap grep t50 php pip python python2 python3 aircrack-ng metasploit-framerork -y
 
 pip install pynput
+pip install slowloris
 
+chmod +x atualizar.sh
+chmod +x requisitos/ipinfo.sh
+chmod +x requisitos/DosConfig.sh
+chmod +x requisitos/slowconfig.sh
+chmod +x requisitos/t50config.sh
+chmod +x requisitos/SpamBot.py
+chmod +x requisitos/WifiDos.sh
 chmod +x Ghost007.sh
-
-cd requisitos/
-
-chmod +x *
-
-cd creat-backdoor/
-
-chmod +x back7.sh
 
 clear
 
