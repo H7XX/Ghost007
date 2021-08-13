fd55b7538ff79068d0f2c676246ec3fbc85d5496
H7XX <88547689+H7XX@users.noreply.github.com>
2021-08-13 14:04:13 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,19 +2,16 @@
 
 echo "instalando. . ."
 
-sudo apt-get install figlet curl nmap grep t50 php pip python python2 python3 aircrack-ng zenity metasploit -y
+sudo apt-get install figlet curl nmap grep t50 php pip python python2 python3 aircrack-ng zenity hydra -y
 
 pip install pynput
 pip install slowloris
 
-chmod +x requisitos/atualizar.sh
-chmod +x requisitos/ipinfo.sh
-chmod +x requisitos/DosConfig.sh
-chmod +x requisitos/slowconfig.sh
-chmod +x requisitos/t50config.sh
-chmod +x requisitos/SpamBot.py
-chmod +x requisitos/WifiDos.sh
 chmod +x Ghost007.sh
+cd requisitos/
+chmod +x *
+chmod +x creat-backdoor/back7.sh
+cd ..
 
 echo "Instalação das requisições completa"
 
