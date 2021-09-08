c332f74716f40ace2b60445db9b07ae6e547a993
H7XX <88547689+H7XX@users.noreply.github.com>
2021-09-08 13:42:41 -0300

    Add files via upload

---
+++ requisitos/creat-backdoor/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+chmod +x creat-backdoor/back.sh
+
+echo "Vc ja tem o metasploit instalado?"
+read -p "s/n >> " p1
+case "$p1" in
+	s)
+		echo "instalando..."
+		apt-get install figlet dex2jar -y
+		clear
+	;;
+
+	n)
+		echo "instalando..."
+		apt-get install figlet dex2jar metasploit-framework -y
+                clear
+	;;
+esac
