974cbe659e31734d4528dd9986ed6db6129ed3bd
H7XX <88547689+H7XX@users.noreply.github.com>
2021-09-08 13:43:59 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-echo "instalando. . ."
+echo "instalando..."
 
-sudo apt-get install figlet curl nmap grep t50 php pip python python2 python3 aircrack-ng zenity hydra perl -y
+sudo apt-get install figlet curl nmap grep t50 php pip python python2 python3 aircrack-ng hydra perl -y
 
 pip install pynput
 
 chmod +x Ghost007.sh
 cd requisitos/
 chmod +x *
-chmod +x creat-backdoor/back7.sh
+chmod +x creat-backdoor/install.sh
+bash creat-backdoor/install.sh
 cd ..
 
 echo "Instalação das requisições completa"
 echo "para executar digite: ./Ghost007.sh"
-zenity --info --title="Finalizada" --text="Instalação completa clique em ok para continuar"
